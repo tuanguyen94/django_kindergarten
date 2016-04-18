e359366645edaf3225ba9960307d1ab8ed38e385
tuanguyen94 <tuanguyen94@gmail.com>
2016-04-18 11:07:03 +0700

    add background to model and solve display img

--- kidGames/models.py
+++ kidGames/models.py
@@ -2,9 +2,33 @@ from __future__ import unicode_literals
 import os
 from django.db import models
 
+MEDIA_URL = 'static/media/'
+
 
 def get_image_path(instance, filename):
     return os.path.join('photos', str(instance.id), filename)
+
+
+from uuid import uuid4
+from django.utils.deconstruct import deconstructible
+
+@deconstructible
+class UploadToPathAndRename(object):
+
+    def __init__(self, path):
+        self.sub_path = path
+
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        # get filename
+        if instance.pk:
+            filename = '{}.{}'.format(instance.pk, ext)
+        else:
+            # set filename as random string
+            filename = '{}.{}'.format(uuid4().hex, ext)
+        # return the whole path to the file
+        return os.path.join(self.sub_path, filename)
+
 # Create your models here.
 
 class user(models.Model):
@@ -27,15 +51,16 @@ class question(models.Model):
 	category_id = models.SmallIntegerField(blank=False)
 	question_string = models.CharField(max_length=120,blank=False, null=True)
 	question_number = models.SmallIntegerField(blank=False)
-	img1 = models.ImageField(upload_to=get_image_path, blank=True, null=True)
-	img2 = models.ImageField(upload_to=get_image_path, blank=True, null=True)
-	img3 = models.ImageField(upload_to=get_image_path, blank=True, null=True)
-	img4 = models.ImageField(upload_to=get_image_path, blank=True, null=True)
-	img5 = models.ImageField(upload_to=get_image_path, blank=True, null=True)
-	img6 = models.ImageField(upload_to=get_image_path, blank=True, null=True)
-	img7 = models.ImageField(upload_to=get_image_path, blank=True, null=True)
-	img8 = models.ImageField(upload_to=get_image_path, blank=True, null=True)
-	img9 =  models.ImageField(upload_to=get_image_path, blank=True, null=True)
+	background = img1 = models.ImageField(upload_to=UploadToPathAndRename(os.path.join(MEDIA_URL, 'upload', 'here')), blank=True, null=True)
+	img1 = models.ImageField(upload_to=UploadToPathAndRename(os.path.join(MEDIA_URL, 'upload', 'here')), blank=True, null=True)
+	img2 = models.ImageField(upload_to=UploadToPathAndRename(os.path.join(MEDIA_URL, 'upload', 'here')), blank=True, null=True)
+	img3 = models.ImageField(upload_to=UploadToPathAndRename(os.path.join(MEDIA_URL, 'upload', 'here')), blank=True, null=True)
+	img4 = models.ImageField(upload_to=UploadToPathAndRename(os.path.join(MEDIA_URL, 'upload', 'here')), blank=True, null=True)
+	img5 = models.ImageField(upload_to=UploadToPathAndRename(os.path.join(MEDIA_URL, 'upload', 'here')), blank=True, null=True)
+	img6 = models.ImageField(upload_to=UploadToPathAndRename(os.path.join(MEDIA_URL, 'upload', 'here')), blank=True, null=True)
+	img7 = models.ImageField(upload_to=UploadToPathAndRename(os.path.join(MEDIA_URL, 'upload', 'here')), blank=True, null=True)
+	img8 = models.ImageField(upload_to=UploadToPathAndRename(os.path.join(MEDIA_URL, 'upload', 'here')), blank=True, null=True)
+	img9 = models.ImageField(upload_to=UploadToPathAndRename(os.path.join(MEDIA_URL, 'upload', 'here')), blank=True, null=True)
 	answeer = models.SmallIntegerField(blank=False)
 
 	def __unicode__(self): #__str__
--- kidGames/views.py
+++ kidGames/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from kidGames.models import question
+
 def index(request):
     #return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request,"home.html",{})
+    mystring = "this is a string"
+    data = question.objects.all();
+    return render(request,"home.html",{ "data": data})
